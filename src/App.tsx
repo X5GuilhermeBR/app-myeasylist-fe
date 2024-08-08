404fb2fd186fee22256a5d6dfc72ca552795603f
Guilherme José <gui_guifonseca@hotmail.com>
2024-08-08 00:14:16 -0300

    MEL: Created page to welcome

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <>
+          <img src="https://i.gifer.com/ZZ5E.gif" className="logo react" />
+        </>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>My Easy List</h1>
+      <div>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Em breve versão DEMO...
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Projeto desenvolvido por Guilherme José
       </p>
     </>
   )
